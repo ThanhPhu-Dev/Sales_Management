1c1f81ac5e3d2a9127042da820a984b73c8c5136
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-06-10 10:45:50 +0700

    UI them khách hàng

--- src/main/java/SalesManagement/controller/CustomerController.java
+++ src/main/java/SalesManagement/controller/CustomerController.java
@@ -6,8 +6,10 @@
 package SalesManagement.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -20,6 +22,16 @@ public class CustomerController {
     @RequestMapping(value = "/customer", method = RequestMethod.GET)
     public ModelAndView customerPage() {
         ModelAndView mav = new ModelAndView("Customer");
+        return mav;
+    }
+    @RequestMapping(value = "/customer/add", method = RequestMethod.GET)
+    public ModelAndView addCustomerPage() {
+        ModelAndView mav = new ModelAndView("AddCustomer");
+        return mav;
+    }
+    @RequestMapping(value = "/customer/update/{Id}", method = RequestMethod.GET)
+    public ModelAndView updateCustomerPage(@PathVariable int Id) {
+        ModelAndView mav = new ModelAndView("UpdateCustomer", "id", Id);
         return mav;
     }
 }
---
+++ src/main/webapp/WEB-INF/views/UpdateCustomer.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : UpdateCustomer
+    Created on : Jun 10, 2021, 8:22:52 AM
+    Author     : NghiaDX
+--%>
+
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+        <title>Cap nhat</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <h1>
+             ${id}
+        </h1>
+    </body>
+</html>
