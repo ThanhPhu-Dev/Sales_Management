60a37ad557e5ed7b22bc55d60774b4e061c4ad82
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-06-13 08:54:52 +0700

    binding customerHome

--- src/main/java/SalesManagement/controller/CustomerController.java
+++ src/main/java/SalesManagement/controller/CustomerController.java
@@ -5,6 +5,10 @@
  */
 package SalesManagement.controller;
 
+import SalesManagement.dao.CustomerDAO;
+import SalesManagement.dto.Customer;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +23,23 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class CustomerController {
 
+    @Autowired
+    CustomerDAO customdfgerDAO;
+
     @RequestMapping(value = "/customer", method = RequestMethod.GET)
     public ModelAndView customerPage() {
-        ModelAndView mav = new ModelAndView("Customer");
+        List<Customer> cusList = customdfgerDAO.findAllCustomerBy();
+        
+        ModelAndView mav = new ModelAndView("Customer", "customers", cusList);
         return mav;
     }
+
     @RequestMapping(value = "/customer/add", method = RequestMethod.GET)
     public ModelAndView addCustomerPage() {
         ModelAndView mav = new ModelAndView("AddCustomer");
         return mav;
     }
+
     @RequestMapping(value = "/customer/update/{Id}", method = RequestMethod.GET)
     public ModelAndView updateCustomerPage(@PathVariable int Id) {
         ModelAndView mav = new ModelAndView("UpdateCustomer", "id", Id);
--- src/main/java/SalesManagement/dao/CustomerDAO.java
+++ src/main/java/SalesManagement/dao/CustomerDAO.java
@@ -1,19 +1,37 @@
 package SalesManagement.dao;
 
 import SalesManagement.dto.Customer;
+import java.sql.*;
+import java.util.*;
 import lombok.Data;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Data
 @Repository
 public class CustomerDAO {
+
     private JdbcTemplate template;
 
     public Customer findCustomerById(int id) {
         String sql = "select * from Customers where Id=?";
-        return template.queryForObject(sql, new Object[]{id}, new BeanPropertyRowMapper<Customer>(Customer.class));
+        return template.queryForObject(sql, new Object[]{id}, new BeanPropertyRowMapper<>(Customer.class));
     }
 
+    public List<Customer> findAllCustomerBy() {
+        return template.query("select * from Customers", new RowMapper<Customer>() {
+            public Customer mapRow(ResultSet rs, int row) throws SQLException {
+                Customer e = new Customer();
+                e.setId(rs.getInt(1));
+                e.setName(rs.getString(2));
+                e.setNumberCard(rs.getString(3));
+                e.setAccountBalance(rs.getInt(4));
+                e.setPromotionsId(rs.getInt(5));
+                e.setDebtMax(rs.getInt(6));
+                return e;
+            }
+        });
+    }
 }
