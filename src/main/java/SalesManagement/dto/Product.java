3bbdb14cb635ab09e0f54c6379fd08fc43174fce
Dương Trọng Phúc <dtrongphucdv@gmail.com>
2021-06-15 21:17:59 +0700

    link to promotions

--- src/main/java/SalesManagement/dto/Product.java
+++ src/main/java/SalesManagement/dto/Product.java
@@ -10,5 +10,16 @@ public class Product {
     private Integer HistoricalCost;
     private Float TradeDiscount;
     private Integer PromotionsId;
+    private PromotionsProduct Promotions;
+
+    // Phương thức lấy giá bán của sản phẩm
+    // Lưu ý rằng giá bán này còn có thể giảm dựa vào ưu đãi của khách hàng.
+    public Float getProductSalePrice() {
+        // Kiểm tra có ưu đãi và tính
+        Float sale = Promotions != null ?  HistoricalCost * (Promotions.getPercentDiscount()/100) : 0;
+        // Giá bán = giá gốc - (chiết khấu + ưu đãi)
+        return HistoricalCost - (HistoricalCost * (TradeDiscount/100) + sale);
+    }
+
     //tạo getter - setter ngầm rồi. bên ngoài gọi getter-setter như bình thường
 }
