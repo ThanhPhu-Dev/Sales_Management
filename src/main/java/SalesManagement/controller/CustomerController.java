050a0609f3e15c345b335271646d04bf060cd59a
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-06-15 14:31:33 +0700

    call api to method get CustomerView

--- src/main/java/SalesManagement/controller/CustomerController.java
+++ src/main/java/SalesManagement/controller/CustomerController.java
@@ -20,6 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,15 +50,13 @@ public class CustomerController {
         ModelAndView mav = new ModelAndView("Customer/Customer", "customers", cusList);
         return mav;
     }
-    
-    @PostMapping("/customer")
-    public ModelAndView customerSearch() {
-        List<Customer> cusList = customerDAO.findAllCustomerBy("");
+    @RequestMapping(value = "/customer/search", method = RequestMethod.GET)
+    public ModelAndView customerSearch(@RequestParam String name) {
+        List<Customer> cusList = customerDAO.findAllCustomerBy(name);
         
         ModelAndView mav = new ModelAndView("Customer/Customer", "customers", cusList);
         return mav;
     }
-    
     @RequestMapping(value = "/customer/add", method = RequestMethod.GET)
     public ModelAndView addCustomerPage() {
         List<PromotionsCustomer> cusList = proCustomerDAO.findAllPromitionsCustomer();
