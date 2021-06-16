a4cba329af167ac26b63958f82ea2d4e06108661
Dương Trọng Phúc <dtrongphucdv@gmail.com>
2021-06-16 21:02:33 +0700

    Update ProductDAO.java

--- src/main/java/SalesManagement/dao/ProductDAO.java
+++ src/main/java/SalesManagement/dao/ProductDAO.java
@@ -21,7 +21,21 @@ public class ProductDAO {
 
     public Product findProductById(int id) {
         String sql = "select * from products where Id=?";
-        return template.queryForObject(sql, new Object[]{id}, new BeanPropertyRowMapper<>(Product.class));
+        return template.queryForObject(sql, new Object[]{id}, new RowMapper<Product>() {
+            @Override
+            public Product mapRow(ResultSet resultSet, int i) throws SQLException {
+                Product product = new Product();
+                product.setId(resultSet.getInt("Id"));
+                product.setName(resultSet.getString("Name"));
+                product.setSpecification(resultSet.getInt("Specification"));
+                product.setHistoricalCost(resultSet.getInt("HistoricalCost"));
+                product.setPromotionsId(resultSet.getInt("PromotionId"));
+                product.setTradeDiscount(resultSet.getFloat("TradeDiscount"));
+                product.setPromotions(product.getPromotionsId() > 0 ?
+                        promotionsProductDAO.findPromotionById(product.getPromotionsId()) : null);
+                return product;
+            }
+        });
     }
 
     public List<Product> getProducts() {
